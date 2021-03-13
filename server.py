a7d5e7d11b94a6fa28367d518e97f297ebfa70d9
rosenrose <42435748+rosenrose@users.noreply.github.com>
2021-03-13 13:38:55 +0900

    개선

--- server.py
+++ server.py
@@ -7,7 +7,23 @@ app = Flask(__name__)
 
 headers = {"User-Agent": "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36"}
 galleries = [("디제이맥스","djmaxrespect"),("사이터스","rayarkcytus"),("지브리","ghibli"),("하프라이프","halflife3"),("보이즈","voezvalkyrie"),("디모","deemo"),("동방","touhou")]
-regex = [("(노|놐|누|눜)(.?)","냐\g<2>"),("(노|놐|누|눜|이기)([.,!?;ㄱ-ㅎ]*)$","냐\g<2>"),("되노","되냐"),("무친","미친"),("무쳤","미쳤"),("노무","너무"),("운지","좆망")]
+regex = [(r"(노|놐|누|눜)([.,!?;ㄱ-ㅎ]*\s+)","냐\g<2>"),(r"(노|놐|누|눜|이기)([.,!?;ㄱ-ㅎ]*)$","냐\g<2>"),("되노","되냐"),("뭐노","뭐냐"),("무친","미친"),("무쳤","미쳤"),("노무","너무"),("운지","좆망"),("(했|햇)(노|누)","했냐")]
+
+def normalize(url):
+    response = requests.get(url, headers=headers)
+    content = response.content.decode("utf-8")
+    content = content.replace("https://m.dcinside.com/board","")
+    soup = BeautifulSoup(content, "html.parser")
+    for s in soup.select("script"): s.decompose()
+    textNodes = [i for i in soup.find_all(string=True) if i.strip() and (i.parent.name not in ['style', 'script', 'head', 'title', 'meta', '[document]'])]
+    for text in textNodes:
+        for reg in regex:
+            compiled = re.compile(reg[0])
+            if result := compiled.search(text):
+                # print(result[0]," || ",compiled.sub(reg[1],text))
+                text.replace_with(compiled.sub(reg[1],text))
+    return soup
+
 @app.route('/')
 def index():
     return render_template("gallery.html", galleries=galleries)
@@ -15,26 +31,17 @@ def index():
 @app.route('/<id>')
 def gall(id):
     params = "&".join([f"{i[0]}={i[1]}" for i in request.args.items()])
-    response = requests.get(f"https://m.dcinside.com/board/{id}?{params}", headers=headers)
-    content = response.content.decode("utf-8")
-    for reg in regex:
-        content = re.compile(reg[0]).sub(reg[1],content)
-    soup = BeautifulSoup(content, "html.parser")
-    for s in soup.select("script"): s.decompose()
-    for a in soup.select("a"):
-        a["href"] = a["href"].replace("https://m.dcinside.com/board","")
+    soup = normalize(f"https://m.dcinside.com/board/{id}?{params}")
+    soup.select("ul.tab-lst")[1].select_one("li:nth-child(2) > a")["href"] = f"/{id}?recommend=1"
     return str(soup)
 
 @app.route('/<id>/<no>')
 def doc(id, no):
-    response = requests.get(f"https://m.dcinside.com/board/{id}/{no}", headers=headers)
-    content = response.content.decode("utf-8")
-    for reg in regex:
-        content = re.compile(reg[0]).sub(reg[1],content)
-    soup = BeautifulSoup(content, "html.parser")
-    for s in soup.select("script"): s.decompose()
-    for a in soup.select("a"):
-        a["href"] = a["href"].replace("https://m.dcinside.com/board","")
+    params = "&".join([f"{i[0]}={i[1]}" for i in request.args.items()])
+    soup = normalize(f"https://m.dcinside.com/board/{id}/{no}?{params}")
+    for img in soup.select("div.gall-thum-btm-inner img"):
+        if img.has_attr("data-original"):
+            img["src"] = img["data-original"]
     return str(soup)
 
 if __name__ == '__main__':
