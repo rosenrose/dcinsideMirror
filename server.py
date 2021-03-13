f2a333d6e7d7b72d2ea38a54408d91ba728108f9
rosenrose <42435748+rosenrose@users.noreply.github.com>
2021-03-13 22:49:20 +0900

    원본 링크

--- server.py
+++ server.py
@@ -48,6 +48,12 @@ def doc(id, no):
     for img in soup.select("div.gall-thum-btm-inner img"):
         if img.has_attr("data-original"):
             img["src"] = img["data-original"]
+    title = soup.select_one("div.gallview-tit-box > span.tit")
+    title.name = "a"
+    title["href"] = f"https://m.dcinside.com/board/{id}/{no}?{params}"
+    # a = soup.new_tag("a", href=f"https://m.dcinside.com/board/{id}/{no}?{params}")
+    # a.string = f"{id}/{no}"
+    # title.append(a)
     return str(soup)
 
 if __name__ == '__main__':
